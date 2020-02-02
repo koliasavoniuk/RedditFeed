d8da7bd130373a575cc40863f480e6645bab31dd
Mykola Savoniuk <m.savoniuk@gmail.com>
2020-02-02 18:11:22 +0200

    Initial project setup

---
+++ RedditFeed/RedditFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditFeed
+//
+//  Created by Mykola Savoniuk on 02.02.2020.
+//  Copyright © 2020 Mykola Savoniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
